82b0b1d863d90a4345e640c2daa24a17995b79ad
nikhil gupta <guptanikhil9755@gmail.com>
2023-12-26 14:04:10 +0530

    create pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import Home from './pages/Home'
+import SignIn from './pages/SignIn'
+import SignOut from './pages/SignOut'
+import Profile from './pages/Profile'
+import About from './pages/About'
 
 function App() {
   return (
-    <div className='text-red-700'>App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/> } />
+        <Route path='/sing-in' element={<SignIn/> } />
+        <Route path='/sing-out' element={<SignOut/> } />
+        <Route path='/about' element={<About/> } />
+        <Route path='/profile' element={<Profile/> } />
+        
+      </Routes>
+    </BrowserRouter>
   )
 }
 
