504e2b77416f8f5ff49b48adf765ff36060d2362
nikhil gupta <guptanikhil9755@gmail.com>
2024-02-01 01:33:18 +0530

    create sing up api route

---
+++ api/routes/auth.route.js
@@ -0,0 +1,9 @@
+import express from 'express';
+import { singup } from '../controllers/auth.controller.js';
+
+const router=express.Router();
+
+router.post("/singup",singup);
+
+export default router;
+
