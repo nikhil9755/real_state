acecfebd81c4519f91f24870e85bae7adf46df71
nikhil gupta <guptanikhil9755@gmail.com>
2024-03-22 13:05:36 +0530

    create and add oauth component

---
+++ client/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: "mern-estate-ba667.firebaseapp.com",
+  projectId: "mern-estate-ba667",
+  storageBucket: "mern-estate-ba667.appspot.com",
+  messagingSenderId: "329388111193",
+  appId: "1:329388111193:web:2f5730a7703bf7b2948848"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
 import { Link,useNavigate } from 'react-router-dom'
 import { signInStart,signInFailure,signInSuccess } from '../redux/user/userSlice';
+import OAuth from '../components/OAuth';
 
 export default function SignIp() {
   const[formdata,setformdata]=useState({});
@@ -56,6 +57,7 @@ export default function SignIp() {
 
       <input type='password' className='border p-3 rounded-lg' placeholder='Password' name='username' id='password' onChange={handleChange} />
       <button disabled={loading} className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>{loading ? 'Loading...' : 'Sign In'}</button>
+      <OAuth></OAuth>
     </form>
     <div className='flex gap-2 mt-5'>
         <p>Dont Have an account?</p>
