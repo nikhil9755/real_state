0fd99ef0e138013b078944ce7b391b2586532bd2
nikhil gupta <guptanikhil9755@gmail.com>
2024-06-30 09:30:00 +0530

    create get user listing api user route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,54 +1,69 @@
 import { errorHandler } from "../utils/error.js";
-import User from '../models/user.model.js'
+import User from "../models/user.model.js";
+import Listing from "../models/listing.model.js";
 
-export const test=(req,res)=>{
-    res.json({
-        message : 'api route is working',
-    })
+export const test = (req, res) => {
+  res.json({
+    message: "api route is working",
+  });
 };
 
-export const updateUser=async (req,res,next)=>{
-    if(req.user.id!==req.params.id)return next(errorHandler(401,"you can only update your own account"));
+export const updateUser = async (req, res, next) => {
+  if (req.user.id !== req.params.id)
+    return next(errorHandler(401, "you can only update your own account"));
 
-    try {
-        if(req.body.passwrod)
-        req.body.passwrod=bcryptjs.hashSync(req.body.passwrod,10)
+  try {
+    if (req.body.passwrod)
+      req.body.passwrod = bcryptjs.hashSync(req.body.passwrod, 10);
 
-        const updateUser=await User.findByIdAndUpdate(req.params.id,{
-            $set:{
-                username:req.body.username,
-                email:req.body.email,
-                password:req.body.password,
-                avatar:req.body.avatar,
-            }
-        },{new: true})
+    const updateUser = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: {
+          username: req.body.username,
+          email: req.body.email,
+          password: req.body.password,
+          avatar: req.body.avatar,
+        },
+      },
+      { new: true }
+    );
 
-        const {password, ...rest}=updateUser._doc
+    const { password, ...rest } = updateUser._doc;
 
-        res.status(200).json(rest);
+    res.status(200).json(rest);
+  } catch (error) {
+    next(error);
+  }
+};
 
-    } catch (error) {
-        next(error)
-        
+export const deleteUser = async (req, res, next) => {
+  if (req.user.id !== req.params.id)
+    return next(errorHandler(401, "you can only delete your own account"));
+
+  try {
+    if (req.body.passwrod)
+      req.body.passwrod = bcryptjs.hashSync(req.body.passwrod, 10);
+
+    const deleteUser = await User.findByIdAndDelete(req.params.id);
+    res.clearCookie("access_token");
+    res.status(200).json("User has been deleted");
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const userListing = async (req, res, next) => {
+  try {
+    // console.log(req.params.id);
+    if (req.user.id == req.params.id) {
+      const listings = await Listing.find({ userRef: req.params.id });
+      // console.log(listings);
+      res.status(200).json(listings);
+    } else {
+      next(err);
     }
-    
-}
-
-
-export const deleteUser=async (req,res,next)=>{
-    if(req.user.id!==req.params.id)return next(errorHandler(401,"you can only delete your own account"));
-
-    try {
-        if(req.body.passwrod)
-        req.body.passwrod=bcryptjs.hashSync(req.body.passwrod,10)
-
-        const deleteUser=await User.findByIdAndDelete(req.params.id)
-        res.clearCookie('access_token');
-        res.status(200).json("User has been deleted");
-
-    } catch (error) {
-        next(error)
-        
-    }
-    
-}
+  } catch (err) {
+    return next(errorHandler(401, "You can only view your own listing"));
+  }
+};
--- api/models/listing.model.js
+++ api/models/listing.model.js
@@ -1,55 +1,55 @@
 import mongoose from "mongoose";
 
-const listingSchema=new mongoose.Schema(
-    {
-        name:{
-            type: String,
-            required: true
-        },
-        description:{
-            type: String,
-            required: true
-        },
-        address:{
-            type: String,
-            required: true
-        },
-        regularPrice:{
-            type: Number,
-            required: true
-        },
-        discountPrice:{
-            type: String,
-            required: true
-        },
-        bathrooms : {
-            type:Number,
-            required: true,
-        },
-        parking: {
-            type: Boolean,
-            required : true,
-        },
-        type:{
-            type:String,
-            required:true,
-        },
-        offer:{
-            type: Boolean,
-            required: true,
-        },
-        imageUrls:{
-            type:Array,
-            required:true,
+const listingSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    regularPrice: {
+      type: Number,
+      required: true,
+    },
+    discountPrice: {
+      type: String,
+      required: true,
+    },
+    bathrooms: {
+      type: Number,
+      required: true,
+    },
+    parking: {
+      type: Boolean,
+      required: true,
+    },
+    type: {
+      type: String,
+      required: true,
+    },
+    offer: {
+      type: Boolean,
+      required: true,
+    },
+    imageUrls: {
+      type: Array,
+      required: true,
+    },
+    userRef: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
 
-        },
-        userRef:{
-            type: String,
-            required:true,
-        },
-    },{timestamps: true}
-)
-
-const Listing=mongoose.model('Listing',listingSchema);
+const Listing = mongoose.model("listings", listingSchema);
 
 export default Listing;
